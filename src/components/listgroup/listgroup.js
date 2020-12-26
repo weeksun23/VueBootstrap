0ab97c1172899eeeb8f49287d78d7483b072e601
linyz <linyz@viomi.com.cn>
2020-12-26 11:15:37 +0800

    update select

--- src/components/listgroup/listgroup.js
+++ src/components/listgroup/listgroup.js
@@ -2,14 +2,34 @@ import Vue from 'vue';
 import './listgroup.css';
 const tpl = `<div class='list-group vb-list-group' v-show='show'
   :style="{left : left + 'px',top : top + 'px',width : width,'max-height' : maxHeight + 'px'}">
-  <a v-for="el in data" href="javascript:void(0)" class="list-group-item" @click="clickItem(el)">{{el.text}}</a>
+  <a v-for="el in data" href="javascript:void(0)" class="list-group-item" @click="clickItem(el)"
+    :class="{active : el.selected}">
+    {{el.text}}
+  </a>
 </div>`;
+const defaultItemAttrs = {
+  selected : false,
+  text : '',
+  value : null
+};
+function initData(data){
+  for(let i=0,ii;ii=data[i++];){
+    for(let key in defaultItemAttrs){
+      if(ii[key] === undefined){
+        ii[key] = defaultItemAttrs[key];
+      }
+    }
+  }
+}
 const Listgroup = {
   init(data){
+    initData(data);
     let comp = Vue.extend({
       template: tpl,
       //所绑定的select vm
       selectVm : null,
+      //上一次选择的项
+      lastSelectItem : null,
       data(){
         return {
           data : data,
@@ -28,6 +48,13 @@ const Listgroup = {
         },
         clickItem(el){
           this.show = false;
+          let {lastSelectItem,selectVm} = this.$options;
+          if(lastSelectItem === el) return;
+          if(lastSelectItem){
+            lastSelectItem.selected = false;
+          }
+          el.selected = true;
+          this.$options.lastSelectItem = el;
           this.$options.selectVm.setItem(el);
         }
       }
--- src/components/select/select.vue
+++ src/components/select/select.vue
@@ -12,6 +12,9 @@ export default {
       text : ''
     }
   },
+  //绑定的列表项组vm
+  listgroupVm : null,
+  bodyClickHandler : null,
   props : {
     cls : {type : String,default : ''},
     placeholder : {type : String,default : ''},
@@ -24,9 +27,16 @@ export default {
     this.$options.listgroupVm = listgroupVm;
   },
   beforeDestroy(){
+    this.removeBodyClick();
     this.$options.listgroupVm.destroy();
   },
   methods : {
+    removeBodyClick(){
+      if(this.$options.bodyClickHandler){
+        document.body.removeEventListener('click',this.$options.bodyClickHandler);
+        this.$options.bodyClickHandler = null;
+      }
+    },
     focus(){
       if(this.data.length === 0) return;
       let listgroupVm = this.$options.listgroupVm;
@@ -36,6 +46,18 @@ export default {
       listgroupVm.left = box.left;
       listgroupVm.top = box.height + box.top;
       listgroupVm.width = box.width + 'px';
+      this.removeBodyClick();
+      this.$options.bodyClickHandler = (e) => {
+        if(e.target === el) return;
+        let result = DomUtil.findTargetParent(e.target,(node) => {
+          if(node === listgroupVm.$el) return true;
+        });
+        if(!result){
+          listgroupVm.show = false;
+          this.removeBodyClick();
+        }
+      };
+      document.body.addEventListener('click',this.$options.bodyClickHandler);
     },
     setItem(item){
       this.text = item.text;
